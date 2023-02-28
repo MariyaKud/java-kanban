61a214147830c4974c943ddcc29e483e35537b09
MariyaKud <m.kudryashova@tealfox.com>
2023-02-28 09:48:54 +0300

    style: Исправить орфографию в классе Task

--- src/model/SubTask.java
+++ src/model/SubTask.java
@@ -3,14 +3,8 @@ package model;
 public class SubTask extends Issue {
     private final Epic parent; // родитель эпик
 
-    public SubTask(int id, String tittle, String description, Epic parent) {
-        super(id, tittle, description);
-        this.parent = parent;
-        parent.addChild(this);
-    }
-
-    public SubTask(int id, String tittle, String description, Epic parent, IssueStatus status) {
-        super(id, tittle, description);
+    public SubTask(int id, String title, String description, Epic parent, IssueStatus status) {
+        super(id,title,description);
         this.parent = parent;
         this.status = status;
     }
@@ -21,14 +15,13 @@ public class SubTask extends Issue {
 
     @Override
     public String toString() {
-        return "SubTask{" +
-                "id=" + this.id +
-                ", parentId=" + this.parent.getId() +
-                ", status=" + this.getStatus() +
-                ", tittle='" + this.getTittle() + '\'' +
-                ", hash='" + this.hashCode() + '\'' +
-                ", parent.hash='" + this.parent.hashCode() + '\'' +
+        return "SubTask{"  +
+                "id=" + id +
+                ", parentId=" + parent.getId() +
+                ", status="   + getStatus() +
+                ", title='"   + getTitle() + '\'' +
+                ", hash='"    + hashCode() + '\'' +
+                ", parent.hash='" + parent.hashCode() + '\'' +
                 '}';
     }
-
 }
