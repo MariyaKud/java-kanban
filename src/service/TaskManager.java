d9f80b5f66c4e5097d638459885908b84d4f86d6
MariyaKud <m.kudryashova@tealfox.com>
2023-02-28 10:07:18 +0300

    ref: Добавить в классе TaskManager методы инициализации типов задач (Task, SubTask, Epic) fix: Исправить видимость метода newId() класса TaskManager

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -8,7 +8,7 @@ import model.*;
 public class TaskManager {
 
     // Переменные класса
-    private int id; //доступный идентификатор менеджера
+    private int id = 1; //доступный идентификатор менеджера
     private final HashMap<Integer, Task> tasks = new HashMap<>();
     private final HashMap<Integer, Epic> epics = new HashMap<>();
     private final HashMap<Integer, SubTask> subTasks = new HashMap<>();
@@ -20,70 +20,82 @@ public class TaskManager {
     private final static String msgErrorTaskNUll = "Данные по обновлению задачи пустые.";
     private final static String msgErrorNotFoundEpic = "Менеджер не нашел родителя добавляемой подзадачи.";
 
-    public TaskManager() {
-        this.id = 1;
-    }
-
     /**
      * Метод выдает очередной идентификатор для новой задачи + готовит идентификатор для следующей задачи
      *
      * @return возвращает очередной свободный идентификатор
      */
-    public int newId() {
+    private int newId() {
         return id++;
     }
 
     /**
      * Метод для перезапуска менеджера:
-     * - очищает данные: задачи/подзадачи/эпики
-     * - id = 1
+     * очищает данные: задачи/подзадачи/эпики
+     * id = 1
      */
     public void restartTaskManager() {
         deleteAllTask(IssueType.TASK);
         deleteAllTask(IssueType.EPIC);
-        this.id = 1;
+        id = 1;
+    }
+
+    public Task initTask(String title, String description, IssueStatus status) {
+        return new Task(newId(), title, description, status);
+    }
+
+    public SubTask initSubTask(String title,String description, Epic parent, IssueStatus status) {
+        return new SubTask(newId(), title, description, parent, status);
+    }
+
+    public Epic initEpic(String title,String description) {
+        return new Epic(newId(), title, description);
     }
 
     /**
      * Метод создает задачу. Сам объект передается в качестве параметра.
      *
-     * @param issueType - тип задачи IssueType = {Task, SubTask, Epic}
-     * @param issue     - экземпляр класса Issue
+     * @param issueType тип задачи IssueType = {Task, SubTask, Epic}
+     * @param issue     экземпляр класса Issue
      */
     public void createTask(IssueType issueType, Issue issue) {
 
-        if (issueType == null) {
-            System.out.println(msgErrorTypeNull);
-        } else if (issue == null) {
-            System.out.println(msgErrorTaskNUll);
-        } else {
+        if (issue != null) {
             switch (issueType) {
                 case TASK:
                     tasks.put(issue.getId(), (Task) issue);
                     break;
+
                 case EPIC:
                     epics.put(issue.getId(), (Epic) issue);
-                    //эпик создается без подзадач, поэтому проверка для детей не нужна
                     break;
+
                 case SUBTASK:
-                    Epic parent = ((SubTask) issue).getParent();
+                    SubTask newSubTask = (SubTask) issue;
+                    Epic parent = newSubTask.getParent();
+                    //Если родитель известен, то берем и его ребенка
                     if (epics.containsValue(parent)) {
                         subTasks.put(issue.getId(), (SubTask) issue);
+                        //добавляем родителю ребенка
+                        parent.getChildren().add(newSubTask);
                     } else {
                         System.out.println(msgErrorNotFoundEpic);
                     }
                     break;
+
                 default:
                     System.out.println(msgErrorTypeUnKnow);
             }
+        } else {
+            System.out.println(msgErrorTypeNull);
         }
     }
 
     /**
      * Метод удаляет задачу по идентификатору.
      *
-     * @param issueType - тип задачи IssueType = {Task, SubTask, Epic}
-     * @param idIssue   - идентификатор задачи на удаление
+     * @param issueType тип задачи IssueType = {Task, SubTask, Epic}
+     * @param idIssue   идентификатор задачи на удаление
      */
     public void deleteTask(IssueType issueType, int idIssue) {
 
@@ -95,21 +107,25 @@ public class TaskManager {
                     System.out.println(msgErrorIdNotFound);
                 }
                 break;
+
             case SUBTASK:
                 if (subTasks.containsKey(idIssue)) {
                     SubTask subTask = (SubTask) getTaskForId(issueType, idIssue);
-                    //Удаляем ребенка у эпика
-                    subTask.getParent().deleteChild(subTask);
-                    //Удаляем из менеджера подзадачу
-                    subTasks.remove(idIssue);
+                    if (subTask != null) {
+                        //Удаляем эту подзадачу в эпике
+                        subTask.getParent().getChildren().remove(subTask);
+                        //Удаляем из менеджера подзадачу
+                        subTasks.remove(idIssue);
+                    }
                 } else {
                     System.out.println(msgErrorIdNotFound);
                 }
                 break;
+
             case EPIC:
                 if (epics.containsKey(idIssue)) {
-                    //удаляем подзадачи эпика
-                    for (SubTask child : getSubTaskForEpic(epics.get(idIssue))) {
+                    //удаляем подзадачи эпика в менеджере
+                    for (SubTask child : epics.get(idIssue).getChildren()) {
                         if (subTasks.containsValue(child)) {
                             subTasks.remove(child.getId());
                         }
@@ -119,6 +135,7 @@ public class TaskManager {
                     System.out.println(msgErrorIdNotFound);
                 }
                 break;
+
             default:
                 System.out.println(msgErrorTypeUnKnow);
         }
@@ -127,85 +144,85 @@ public class TaskManager {
     /**
      * Метод для получения списка всех задач заданного типа
      *
-     * @param issueType - тип задачи IssueType = {Task, SubTask, Epic}
+     * @param issueType тип задачи IssueType = {Task, SubTask, Epic}
      * @return возвращает список задач менеджера по заданному типу
      */
     public ArrayList<Issue> getAllTask(IssueType issueType) {
         ArrayList<Issue> issues = new ArrayList<>();
 
-        if (issueType == null) {
-            System.out.println(msgErrorTypeNull);
-        } else if (issueType == IssueType.TASK) {
-            issues.addAll(tasks.values());
-        } else if (issueType == IssueType.EPIC) {
-            issues.addAll(epics.values());
-        } else if (issueType == IssueType.SUBTASK) {
-            issues.addAll(subTasks.values());
-        } else {
-            System.out.println(msgErrorTypeUnKnow);
-        }
+        switch (issueType) {
+            case TASK:
+                issues.addAll(tasks.values());
+                break;
 
+            case EPIC:
+                issues.addAll(epics.values());
+                break;
+
+            case SUBTASK:
+                issues.addAll(subTasks.values());
+                break;
+
+            default:
+                System.out.println(msgErrorTypeUnKnow);
+        }
         return issues;
     }
 
     /**
      * Метод удаляет все задачи по заданному типу
      *
-     * @param issueType - тип задачи IssueType = {Task, SubTask, Epic}
+     * @param issueType тип задачи IssueType = {Task, SubTask, Epic}
      */
     public void deleteAllTask(IssueType issueType) {
         switch (issueType) {
             case TASK:
-                this.tasks.clear();
+                tasks.clear();
                 break;
+
             case EPIC:
-                this.subTasks.clear(); // нет смысла оставлять подзадачи, если родители удалены
-                this.epics.clear();
+                subTasks.clear(); // нет смысла оставлять подзадачи, если родители удалены
+                epics.clear();
                 break;
+
             case SUBTASK:
                 for (Epic epic : epics.values()) {
-                    epic.deleteChildren();
+                    epic.getChildren().clear();
                 }
-                this.subTasks.clear();
+                subTasks.clear();
                 break;
+
             default:
                 System.out.println(msgErrorTypeUnKnow);
         }
     }
 
     /**
-     * Метод возвращает задачу по идентификатору.
+     * Метод возвращает задачу по идентификатору или NULL
      *
-     * @param issueType - тип задачи IssueType = {Task, SubTask, Epic}
-     * @param idIssue   - идентификатор задачи для поиска
-     * @return - задачу по идентификатору, может вернуть null
+     * @param issueType тип задачи IssueType = {Task, SubTask, Epic}
+     * @param idIssue   идентификатор задачи для поиска
+     * @return - задачу по идентификатору, может вернуть NULL
      */
     public Issue getTaskForId(IssueType issueType, int idIssue) {
-
-        if (idIssue < 0 || idIssue > this.id) {
-            System.out.println(msgErrorIdNotFound);
-        } else if (issueType == null) {
-            System.out.println(msgErrorTypeNull);
-        } else {
-            switch (issueType) {
-                case TASK:
-                    return tasks.get(idIssue);
-                case EPIC:
-                    return epics.get(idIssue);
-                case SUBTASK:
-                    return subTasks.get(idIssue);
-                default:
-                    System.out.println(msgErrorTypeUnKnow);
-            }
+        switch (issueType) {
+            case TASK:
+                return tasks.get(idIssue);
+            case EPIC:
+                return epics.get(idIssue);
+            case SUBTASK:
+                return subTasks.get(idIssue);
+            default:
+                System.out.println(msgErrorTypeUnKnow);
+                return null;
         }
-        return null;
     }
 
     /**
      * Метод возвращает список всех подзадач определённого эпика.
      *
-     * @param epic - эпик, по которому нужно получить список подзадач
-     * @return - список подзадач эпика
+     * @param epic эпик, по которому нужно получить список подзадач
+     * @return список подзадач эпика
      */
     public ArrayList<SubTask> getSubTaskForEpic(Epic epic) {
         return epic.getChildren();
@@ -214,29 +231,69 @@ public class TaskManager {
     /**
      * Метод обновляет данные в хранилище по задаче.
      *
-     * @param issueType - тип задачи IssueType = {Task, SubTask, Epic}
-     * @param issue     - новая версия объекта с верным идентификатором, включая обновленный статус
+     * @param issueType тип задачи IssueType = {Task, SubTask, Epic}
+     * @param issue     новая версия объекта с верным идентификатором, включая обновленный статус
      */
     public void updateTask(IssueType issueType, Issue issue) {
+
         if (issue != null) {
             switch (issueType) {
                 case TASK:
-                    tasks.put(issue.getId(), (Task) issue);
+                    Task newTask = (Task) issue;
+                    Task oldTask = (Task) getTaskForId(IssueType.TASK, issue.getId());
+
+                    //Мы можем обновить только существующий объект
+                    if (oldTask != null) {
+                        tasks.put(issue.getId(), newTask);
+                    } else {
+                        System.out.println(msgErrorIdNotFound);
+                    }
                     break;
+
                 case EPIC:
-                    ((Epic) issue).setStatus();
-                    epics.put(issue.getId(), (Epic) issue);
+                    Epic newEpic = (Epic) issue;
+                    Epic oldEpic = (Epic) getTaskForId(IssueType.EPIC, issue.getId());
+
+                    if (oldEpic != null) {
+                        //Удаляем все подзадачи старого эпика
+                        for (SubTask child : oldEpic.getChildren()) {
+                            subTasks.remove(child.getId());
+                        }
+                        //Обновляем эпик
+                        epics.put(issue.getId(), (Epic) issue);
+                        //Устанавливаем статус нового эпика
+                        newEpic.updateStatus();
+                        //Добавляем подзадачи нового эпика
+                        for (SubTask child : newEpic.getChildren()) {
+                            if (subTasks.containsKey(child.getId())) {
+                                updateTask(IssueType.SUBTASK, child);
+                            } else {
+                                subTasks.put(child.getId(),child);
+                            }
+                        }
+                    } else {
+                        System.out.println(msgErrorIdNotFound);
+                    }
                     break;
+
                 case SUBTASK:
-                    //Родитель подзадачи - эпик
-                    Epic parent = ((SubTask) issue).getParent();
-                    //Удаляем из эпика ссылку на старую подзадачу
-                    parent.deleteChild((SubTask) getTaskForId(IssueType.SUBTASK, issue.getId()));
-                    //Обновляем подзадачу в списке подзадач
-                    subTasks.put(issue.getId(), (SubTask) issue);
-                    //Добавляем обновленную подзадачу в эпик
-                    parent.addChild((SubTask) issue);
+                    SubTask newSubTask = (SubTask) issue;
+                    SubTask oldSubTask  = (SubTask) getTaskForId(IssueType.SUBTASK, issue.getId());
+
+                    if (oldSubTask != null) {
+                        //Родитель подзадачи - эпик
+                        Epic parent = oldSubTask.getParent();
+                        //Удаляем старую подзадачу у эпика родителя
+                        parent.getChildren().remove(oldSubTask);
+                        //Обновляем подзадачу в списке подзадач
+                        subTasks.put(issue.getId(), newSubTask);
+                        //Добавляем обновленную подзадачу в эпик
+                        parent.getChildren().add(newSubTask);
+                    } else {
+                        System.out.println(msgErrorIdNotFound);
+                    }
                     break;
+
                 default:
                     System.out.println(msgErrorTypeUnKnow);
             }
@@ -244,5 +301,4 @@ public class TaskManager {
             System.out.println(msgErrorTaskNUll);
         }
     }
-
 }
