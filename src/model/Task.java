777c46d66fc10d00f86dba225f3fbcc3596b8b54
MariyaKud <m.kudryashova@tealfox.com>
2023-02-28 09:48:31 +0300

    style: Исправить орфографию в классе Task

--- src/model/Task.java
+++ src/model/Task.java
@@ -2,23 +2,18 @@ package model;
 
 public class Task extends Issue {
 
-    public Task(int id, String tittle, String description) {
-        super(id, tittle, description);
-    }
-
-    public Task(int id, String tittle, String description, IssueStatus status) {
-        super(id, tittle, description);
+    public Task(int id, String title, String description, IssueStatus status) {
+        super(id,title,description);
         this.status = status;
     }
 
     @Override
     public String toString() {
         return "Task{" +
-                "id=" + this.id +
-                ", status=" + this.status +
-                ", tittle='" + this.tittle + '\'' +
-                ", hash='" + this.hashCode() + '\'' +
+                "id=" + id +
+                ", status=" + status +
+                ", title='" + title + '\'' +
+                ", hash='"  + hashCode() + '\'' +
                 '}';
     }
-
 }
