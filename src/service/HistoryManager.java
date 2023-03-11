74bfad7f4d213b85a0405c1cfc01504a73b32bb2
MariyaKud <m.kudryashova@tealfox.com>
2023-03-11 23:35:24 +0300

    feat: Добавить новый метод clearHistory

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -4,9 +4,14 @@ import model.Issue;
 
 import java.util.List;
 
+/**
+ * Контракт для объекта-история просмотров
+ */
 public interface HistoryManager {
 
     void add(Issue issue);
+
     List<Issue> getHistory();
+
     void clearHistory();
 }
