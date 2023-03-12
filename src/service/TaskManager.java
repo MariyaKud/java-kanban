c4c7e734d00c97f47be397c66bd1dcde28917486
MariyaKud <m.kudryashova@tealfox.com>
2023-03-12 11:27:06 +0300

    refactor: Перенести строковые константы по ошибкам.

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -19,8 +19,16 @@ public interface TaskManager {
     String MSG_ERROR_ID_NOT_FOUND = "Не найдена задача с указанным id.";
     String MSG_ERROR_WRONG_EPIC = "При обновлении эпика дети остаются неизменными.";
     String MSG_ERROR_NOT_FOUND_EPIC = "Не найден эпик для подзадачи.";
+    String MSG_ERROR_TYPE_NULL = "Для метода не указан тип задачи.";
+    String MSG_ERROR_TASK_EMPTY = "Список задач пуст.";
+
+    //private final static String MSG_ERROR_PARENT_NULL = "Родитель подзадачи не может быть null.";
+    //private final static String MSG_ERROR_TYPE_UN_KNOW = "Для выбранного типа задач не создан обработчик в методе.";
+    //private final static String MSG_ERROR_ID_NULL = "Не найдена задача с указанным id.";
 
     Task addTask(String title, String description, IssueStatus status);
+
+    Task addTask(String title, String description);
 
     SubTask addSubTask(String title, String description, Epic parent, IssueStatus status);
 
