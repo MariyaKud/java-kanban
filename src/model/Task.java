45bdc40feb9aa18aaf76bfeb47c54133c35020ff
MariyaKud <m.kudryashova@tealfox.com>
2023-03-12 11:26:08 +0300

    refactor: Добавить конструктор без статуса для новой задачи.

--- src/model/Task.java
+++ src/model/Task.java
@@ -7,6 +7,11 @@ public class Task extends Issue {
         this.setStatus(status);
     }
 
+    public Task(int id, String title, String description) {
+        super(id,title,description);
+        this.setStatus(IssueStatus.NEW);
+    }
+
     @Override
     public String toString() {
         return super.toString();
