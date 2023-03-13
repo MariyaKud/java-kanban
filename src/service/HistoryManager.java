c3688b3b0e7017d7093aaa107818e49d22dcb19c
MariyaKud <m.kudryashova@tealfox.com>
2023-03-13 19:26:40 +0300

    style: Добавить javaDoc

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -1,19 +1,34 @@
 package service;
 
+import model.Epic;
 import model.Issue;
+import model.SubTask;
+import model.Task;
 
 import java.util.List;
 
 /**
- * Контракт для объекта-история просмотров
+ * Контракт для стека с историей просмотров задач типа: {@link Task},{@link SubTask},{@link Epic}
  */
 public interface HistoryManager {
 
+    /**
+     * Размер стека просмотренных пользователем задач
+     */
     byte SIZE_HISTORY_OF_VIEW_ISSUE_LIST = 10;
 
+    /**
+     * Добавить задачу в конец стека
+     */
     void add(Issue issue);
 
+    /**
+     * Получить список задач в стеке
+     */
     List<Issue> getHistory();
 
+    /**
+     * Очистить список задач в стеке
+     */
     void clearHistory();
 }
