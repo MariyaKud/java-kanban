7b6b6f86355ca35b7b853823604956b95017007d
MariyaKud <m.kudryashova@tealfox.com>
2023-04-19 11:47:41 +0300

    feat: Добавить тестирование нового менеджера

--- src/service/FileBackedTasksManager.java
+++ src/service/FileBackedTasksManager.java
@@ -3,11 +3,12 @@ package service;
 import DAO.CSVMakeRepository;
 import DAO.ManagerSaveException;
 import model.Epic;
+import model.Issue;
 import model.SubTask;
 import model.Task;
+import model.IssueStatus;
 
 import java.io.File;
-import java.io.IOException;
 
 public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
 
@@ -24,26 +25,56 @@ public class FileBackedTasksManager extends InMemoryTaskManager implements TaskM
         File newTaskManagerFile = new File(HOME,NAME_FILE);
         FileBackedTasksManager tracker = loadFromFile(newTaskManagerFile);
 
-        /*
+        System.out.println("Загружены данные:");
+        System.out.println("ТАSK:");
+        for (Issue issue : tracker.getAllTasks()) {
+            System.out.println(issue);
+        }
+        System.out.println("SUBТАSK:");
+        for (Issue issue : tracker.getAllSubTasks()) {
+            System.out.println(issue);
+        }
+        System.out.println("EPICS:");
+        for (Issue issue : tracker.getAllEpics()) {
+            System.out.println(issue);
+        }
+        System.out.println("ИСТОРИЯ:");
+        for (Issue issue : tracker.getHistory()) {
+            System.out.println(issue);
+        }
+
+        System.out.println("\nЗапускаем автотест менеджера задач, хранящего данные в файле..");
         Task newTask1 = new Task(0, "Task1", "Description");
         tracker.addTask(newTask1);
+        System.out.println("Добавлена задача: " + newTask1);
         Task newTask2 = new Task(0, "Task2", "Description");
         tracker.addTask(newTask2);
+        System.out.println("Добавлена задача: " + newTask2);
         newTask2.setStatus(IssueStatus.DONE);
         tracker.updateTask(newTask2);
+        System.out.println("Установлен статус DONE для задачи: " + newTask2);
+        System.out.println("Добавили в историю задачу: " + newTask2);
 
         Epic newEpic = new Epic(0, "Epic1", "Description");
         tracker.addEpic(newEpic);
+        System.out.println("Добавлен эпик: " + newEpic);
 
-        SubTask newSubTask1 = new SubTask(0, "SubTask1", "Description", newEpic);
+        SubTask newSubTask1 = new SubTask(0, "SubTask1", "Description", newEpic.getId());
         tracker.addSubTask(newSubTask1);
+        System.out.println("Добавлена подзадача: " + newSubTask1);
+
         newSubTask1.setStatus(IssueStatus.DONE);
         tracker.updateSubTask(newSubTask1);
+        System.out.println("Установлен статус DONE для подзадачи: " + newSubTask1);
+        System.out.println("Добавили в историю подзадачу: " + newSubTask1);
 
-        SubTask newSubTask2 = new SubTask(0, "SubTask2", "Description", newEpic);
+        SubTask newSubTask2 = new SubTask(0, "SubTask2", "Description", newEpic.getId());
         tracker.addSubTask(newSubTask2);
+        System.out.println("Добавлена подзадача: " + newSubTask2);
         tracker.getSubTaskById(newSubTask2.getId());
-         */
+        System.out.println("Добавили в историю подзадачу: " + newSubTask2);
+
+        System.out.println("\nАвто тест завершен.");
     }
 
     /**
