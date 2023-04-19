e560fcbbe2fae228b2c8532c1dcb65f8d0fca589
MariyaKud <m.kudryashova@tealfox.com>
2023-04-19 20:32:53 +0300

    fix: Исправить родителя класса

--- src/DAO/ManagerSaveException.java
+++ src/DAO/ManagerSaveException.java
@@ -3,7 +3,7 @@ package DAO;
 /**
  * Исключение вида IOException
  */
-public class ManagerSaveException extends Exception {
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
     }
