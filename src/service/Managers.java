cc0fa2e0623f7b79d713813f78b1fd8acd4d6c33
MariyaKud <m.kudryashova@tealfox.com>
2023-04-20 18:16:48 +0300

    feat: Добавить получение дефолтного менеджера обменов с файлом csv

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,5 +1,10 @@
 package service;
 
+import dao.CSVMakeRepository;
+import dao.IssueRepository;
+
+import java.io.File;
+
 /**
  * Утилитарный класс <b>{@code Managers}</b> ответственный за получение дефолтных значений
  *
@@ -31,5 +36,17 @@ public class Managers {
      */
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    /**
+     * Получить дефолтный объект-обмена менеджера с файлом csv
+     * @return  объект-экземпляр поддерживающий контрактом {@code IssueRepository} для записи и чтения данных в файл csv
+     */
+    public static IssueRepository getDefaultIssueRepository() {
+        final String DIR_HOME = System.getProperty("user.home");
+        final String CSV_FILE_REPOSITORY = "taskManager.csv";
+        File csvFileRepository = new File(DIR_HOME,CSV_FILE_REPOSITORY);
+
+        return new CSVMakeRepository(csvFileRepository);
     }
 }
