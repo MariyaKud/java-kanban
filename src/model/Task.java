24b07df4fd9419142fb97b585daf5f88ffdbf0f0
MariyaKud <m.kudryashova@tealfox.com>
2023-04-30 16:49:01 +0300

    feat: Добавить тесты HistoryManager: пустая история задач, дублирование, добавление, удаление (начало, середина, конец)

--- src/model/Task.java
+++ src/model/Task.java
@@ -10,14 +10,18 @@ import java.time.LocalDateTime;
  */
 public class Task extends Issue {
 
+    public Task(int id, String title, String description, Duration duration) {
+        super(id, title, description, duration);
+    }
+
+    public Task(int id, String title, String description, Duration duration, LocalDateTime startTime) {
+        super(id, title, description, duration, startTime);
+    }
+
     public Task(int id, String title, String description, Duration duration, LocalDateTime startTime,
                 IssueStatus status) {
         super(id, title, description, duration, startTime);
         this.setStatus(status);
-    }
-
-    public Task(int id, String title, String description, Duration duration) {
-        super(id, title, description, duration);
     }
 
     @Override
--- test/service/HistoryManagerTest.java
+++ test/service/HistoryManagerTest.java
@@ -3,9 +3,11 @@ package service;
 import model.Issue;
 import model.Task;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,29 +16,127 @@ class HistoryManagerTest {
 
     private static HistoryManager historyManager;
 
-    @BeforeAll
-    static void beforeAll() {
-        historyManager = Managers.getDefaultHistory();
+    @BeforeEach
+    void beforeEach() {
+        historyManager = new InMemoryHistoryManager();
     }
 
     @Test
-    void add() {
+    void getHistoryNewTest() {
+        //Для нового экземпляра класса HistoryManager - история это пустой список
+        //Проверяем, что getHistory() вернет не null и размер списка = 0
+        final List<Issue> history = historyManager.getHistory();
+
+        assertNotNull(history, "История null.");
+        assertEquals(0, history.size(), "История не пустая.");
+    }
+
+    @Test
+    void addOneTaskTest() {
         //Проверяем наличие задачи в истории, после применения метода add()
-        Task task = new Task(0, "Test addTask", "Test addTask description", Duration.ofMinutes(10));
+        Task task = new Task(0, "Test addTask", "Description", Duration.ofMinutes(10));
 
         historyManager.add(task);
 
         final List<Issue> history = historyManager.getHistory();
 
-        assertNotNull(history, "История не пустая.");
-        assertEquals(1, history.size(), "История Не пустая.");
+        assertNotNull(history, "История null.");
+        assertEquals(1, history.size(), "Длина очереди не соответствует ожидаемой.");
     }
 
     @Test
-    void remove() {
+    void addOneTaskTwiceCheckDoubleTest() {
+        //Проверяем наличие задачи в истории, после применения метода add()
+        Task task = new Task(0, "Test addTask", "Description", Duration.ofMinutes(10));
+
+        historyManager.add(task);
+        historyManager.add(task);
+
+        final List<Issue> history = historyManager.getHistory();
+
+        assertNotNull(history, "История null.");
+        assertEquals(1, history.size(), "Длина очереди не соответствует ожидаемой.");
     }
 
     @Test
-    void getHistory() {
+    void removeSingleTaskTest() {
+        //Удаляем единственную задачу, проверяем чтобы очередь не превращалась в null и была пустой
+        Task task = new Task(0, "Test remove", "Description", Duration.ofMinutes(10));
+
+        historyManager.add(task);
+        historyManager.remove(0);
+
+        final List<Issue> history = historyManager.getHistory();
+
+        assertNotNull(history, "История null.");
+        assertEquals(0, history.size(), "Длина очереди не соответствует ожидаемой.");
+    }
+
+    @Test
+    void removeFromBeginOfLineTest() {
+        //Удаляем задачу из начала очереди
+        LocalDateTime startTime = LocalDateTime.now();
+
+        Task task1 = new Task(0, "Test", "Description", Duration.ofMinutes(10), startTime);
+        historyManager.add(task1);
+
+        Task task2 = new Task(1, "Test", "Description",
+                               Duration.ofMinutes(10), startTime.plusMinutes(15));
+        historyManager.add(task2);
+
+        historyManager.remove(0);
+
+        final List<Issue> history = historyManager.getHistory();
+
+        assertNotNull(history, "История null.");
+        assertEquals(1, history.size(), "Длина очереди не соответствует ожидаемой.");
+        assertEquals(task2, history.get(0), "Не корректная задача в очереди.");
+    }
+
+    @Test
+    void removeFromEndOfLineTest() {
+        //Удаляем задачу из конца очереди
+        LocalDateTime startTime = LocalDateTime.now();
+
+        Task task1 = new Task(0, "Test", "Description", Duration.ofMinutes(10), startTime);
+        historyManager.add(task1);
+
+        Task task2 = new Task(1, "Test", "Description",
+                Duration.ofMinutes(10), startTime.plusMinutes(15));
+        historyManager.add(task2);
+
+        historyManager.remove(1);
+
+        final List<Issue> history = historyManager.getHistory();
+
+        assertNotNull(history, "История null.");
+        assertEquals(1, history.size(), "Длина очереди не соответствует ожидаемой.");
+        assertEquals(task1, history.get(0), "Не корректная задача в очереди.");
+    }
+
+    @Test
+    void removeFromMiddleOfLineTest() {
+        //Удаляем задачу из середины очереди
+        LocalDateTime startTime = LocalDateTime.now();
+
+        Task task1 = new Task(0, "Test", "Description", Duration.ofMinutes(10), startTime);
+        historyManager.add(task1);
+
+        Task task2 = new Task(1, "Test", "Description",
+                Duration.ofMinutes(10), startTime.plusMinutes(15));
+        historyManager.add(task2);
+
+        Task task3 = new Task(2, "Test", "Description",
+                Duration.ofMinutes(10), startTime.plusMinutes(30));
+        historyManager.add(task3);
+
+        historyManager.remove(1);
+
+        final List<Issue> history = historyManager.getHistory();
+
+        assertNotNull(history, "История null.");
+        assertEquals(2, history.size(), "Длина очереди не соответствует ожидаемой.");
+        assertEquals(task1, history.get(0), "Не корректная задача в начале очереди.");
+        assertEquals(task3, history.get(1), "Не корректная задача в конце очереди.");
     }
 }
